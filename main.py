abf59478b77588352028ea9dd7dcbdfcb82a00e8
Emmanuel (Manny) Moundhala <emmanuelmoundhala@gmail.com>
2025-06-26 15:59:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -57,12 +57,12 @@ def get_nodes_with_attribute_filter_endpoint(request: Request):
         matching_nodes.append(node)
     
     print(f"Data: {matching_nodes}\nTotal: {len(matching_nodes)}\nFilters applied: {attribute_filters}\nJob ID: {job_id}")
-    return {
-      "data": matching_nodes,
-      "total": len(matching_nodes),
-      "filters_applied": attribute_filters,
-      "job_id": job_id
-    }
+    # return {
+    #   "data": matching_nodes,
+    #   "total": len(matching_nodes),
+    #   "filters_applied": attribute_filters,
+    #   "job_id": job_id
+    # }
     
   except Exception as e:
     return Response(
